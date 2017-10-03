cd5108e3cf63db3ac99c28b15d226aaf5dc734d8
xvarria <mchavarria@gmail.com>
2017-10-02 19:31:10 -0600

    Uso de la anotacion WebResult

--- src/com/TimeServer.java
+++ src/com/TimeServer.java
@@ -1,6 +1,7 @@
 package com;
 
 import javax.jws.WebMethod;
+import javax.jws.WebResult;
 import javax.jws.WebService;
 import javax.jws.soap.SOAPBinding;
 import javax.jws.soap.SOAPBinding.Style;
@@ -8,8 +9,15 @@ import javax.jws.soap.SOAPBinding.Style;
 @WebService
 @SOAPBinding(style = Style.RPC)
 public interface TimeServer {
-	@WebMethod String getTimeAsString();
-	@WebMethod long getTimeElapsed();
+	
+	@WebMethod
+	@WebResult(partName = "time_response") //esta anotación cambia el WSDL el tag part name cambia de "return" (default) a los que se indique aquí
+	String getTimeAsString();
+	
+	
+	@WebMethod
+	@WebResult(partName = "time_response")
+	long getTimeElapsed();
 }
 
 //SEI
